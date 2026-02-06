9ae1446ba903cd87b57009933684985bc389c49b
vatavrukshatours <vatavrukshatours@gmail.com>
2026-02-06 19:15:38 +0530

    test number changed

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ document.querySelectorAll('.book-now').forEach(button => {
         const tourName = this.getAttribute('data-tour');
         
         // Your WhatsApp number (Format: CountryCode + Number)
-        const phoneNumber = "911234567890"; 
+        const phoneNumber = "918530940208"; 
         
         // Construct the message
         const message = `${tourName} inquiry`;
